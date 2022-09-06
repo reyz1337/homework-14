702f8bf8cd4365a6be65394c54badb81c4b7d2e8
reyz1337 <andrlav65@gmail.com>
2022-09-06 16:43:48 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+//minimum
+//1
+    let a = 0.1;
+    let b = 0.2;
+    let result = (a+b).toFixed(1);
+    alert(`Result: ${result}`);
+
+//2
+    let c = '1';
+    let d = 2;
+    let result_2 = +(c[0])+d;
+    alert(`Result: ${result_2}`);
+
+//3
+    const onegb = 1000;
+    const mb = 820;
+    let gb = +prompt(`Введіть кількість гігабайт: `);
+    let result_3 = gb*onegb/mb;
+    alert(`На флешку поміщається ${Math.round(result_3)} файлів розміром 820мб`);
+
+//normal
+//1
+    let money = +prompt('Введіть кількість грошей: ');
+    let chocolate = +prompt('Введіть ціну шоколадки: ');
+    let result_4 = Math.floor(money/chocolate);
+    let change = Math.round(money/(chocolate*result_4));
+    alert(`Ви можете придбати ${result_4} шоколадок, Ваша решта ${change}`);
+
+//2
+    let number = prompt('Введіть тризначне число: ');
+    let reverse_number = number[2] + number[1] + number[0];
+    alert(`${reverse_number}`);
+    //доробити
+
+//maximum
+//1
+    const percent = 5;
+    const time = 2;
+    const year = 12;
+    let deposit = +prompt('Введіть суму вкладу: ');
+    let profit = (((percent/year)*time)/100)*deposit;
+    alert(`Сума по нарахованим відсоткам: ${profit.toFixed(2)}`);
+//2
+    // Поверне найбільше значення
+
+});
